6b8263544530d866b06bd4d65503ac253a5fc423
James Halliday <substack@gmail.com>
2010-05-01 01:58:24 -0800

    usage message

--- free-indent.pl
+++ free-indent.pl
@@ -5,11 +5,22 @@ use warnings;
 use strict;
 
 use 5.10.0;
-use Text::Indent;
-use Language::Functional qw/:all/;
-use Tie::File;
+use Getopt::Casual;
 
-my $filename = shift // die "Usage: $0 file";
+my $usage = "Usage: $0 ( --convert | --restore ) OPTIONS file
+    Convert and restore files with vi modelines to your preferred indentation
+    style and back to the original formatting.
+    
+    Where OPTIONS are:
+        --vimrc=file - the location of the vimrc file to use
+            default: ~/.vimrc
+        --et, --expandtab - expand tab preference; overrides vimrc
+        --ts, --tabstop - tab stop preference; overrides vimrc
+";
+$ARGV{"--indent"} or $ARGV{"--unindent"} or die $usage;
+
+use Tie::File;
+my $filename = shift // die $usage;
 tie my @file, 'Tie::File', $filename
     or die "Couldn't open '$filename' failed: $!";
 @file or exit; # don't mess with empty files
@@ -39,7 +50,23 @@ for my $opts (grep defined, map $_ =~ $re, grep defined, @file[0..4]) {
         last;
     }
 }
-%set // exit;
+%set // exit; # only process when modeline is found
+
+# short-form aliases
+$set{tabstop} //= $set{ts};
+$set{expandtab} //= $set{et};
+
+# vim:ts=4:sw=4:tw=80:et
+use Text::Indent;
+my $indent = Text::Indent->new(
+    SpaceChar => $set{expandtab} ? " " : "\t",
+    Spaces => ($set{expandtab} ? $set{tabstop} // 8 : 1),
+    AddNewLine => 0,
+);
+
+for my $line (@file) {
+    # ...
+}
 
 untie @file;
 
