2b79e638a1590acd890ca09b63754bdd801ccadc
James Halliday <substack@gmail.com>
2010-05-01 01:58:53 -0800

    get rid of old source detection

--- free-indent.pl
+++ free-indent.pl
@@ -69,25 +69,3 @@ for my $line (@file) {
 }
 
 untie @file;
-
-__END__
-my %comments = (
-    (map { $_ => qr/^ \s* # \s* (.+)/x } qw/
-        perl pl python py bash csh ksh tcsh sh
-    /),
-    (map { $_ => qr/^ \s* -- \s* (.+)/x } qw/
-        runhaskell runghc runhugs hs
-    /),
-);
-
-use File::Basename qw/basename/;
-
-# guess the comment style based on the file's extension
-my $re = $comments{ lc head[ $filename =~ m/\. ([^.]+) $/x ] };
-
-# unless there is a shebang, in which trumps the extension
-if (my @sh = map basename($_), split /\s+/, head[ $file[0] =~ m/^#!(.+)/ ]) {
-    $re = $comments{ $sh[0] eq "env" ? $sh[1] : $sh[0] } // $re;
-}
-
-$re // exit; # don't mess with unknown files
